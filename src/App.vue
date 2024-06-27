1abf443d98bd1e2a7cdbb6699f57d5687f9f27ee
krls2020 <mail@karelhemza.cz>
2024-06-27 13:28:28 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import TheWelcome from './components/TheWelcome.vue'
+
+
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style>
+
+html {
+  font-family: system-ui, sans-serif;
+  background: #13151a;
+  color: #f7e9ff;
+}
+
+p {
+  color: #e1d1e9;
+}
+
+.link {
+  font-weight: 500;
+  color: #f7e9ff;
+}
+
+.link:hover {
+  color: #e1d1e9;
+}
+
+code {
+  font-family:
+      Menlo,
+      Monaco,
+      Lucida Console,
+      Liberation Mono,
+      DejaVu Sans Mono,
+      Bitstream Vera Sans Mono,
+      Courier New,
+      monospace;
+}
+</style>
